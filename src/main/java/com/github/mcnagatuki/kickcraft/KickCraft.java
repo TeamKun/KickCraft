8f80f2bb8cf83b316918e57254f256805099bfa9
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2021-03-21 21:35:35 +0900

    fix: plugin available

--- src/main/java/com/github/mcnagatuki/kickcraft/KickCraft.java
+++ src/main/java/com/github/mcnagatuki/kickcraft/KickCraft.java
@@ -1,5 +1,6 @@
 package com.github.mcnagatuki.kickcraft;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,6 +16,8 @@ public final class KickCraft extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
+
+        this.getServer().getPluginManager().registerEvents(this, this);
         this.getCommand("kickcraft").setExecutor(new CommandManager());
     }
 
@@ -29,6 +32,15 @@ public final class KickCraft extends JavaPlugin implements Listener {
         if (!(damagerEntity instanceof Player)) return;
 
         Player damagedPlayer = (Player) damagedEntity;
+        Player damagerPlayer = (Player) damagerEntity;
+
+        String message = String.format(
+                "%sは%sにキックされたのでキックされました",
+                ChatColor.GOLD + damagedPlayer.getName() + ChatColor.RESET,
+                ChatColor.DARK_GREEN + damagerPlayer.getName() + ChatColor.RESET
+                );
+        getServer().broadcastMessage(message);
+
         damagedPlayer.kickPlayer("キックされたためキックされました");
     }
 
